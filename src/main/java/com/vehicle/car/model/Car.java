1c31ca599c1cdeaa3f917fd146a2d805c3540767
ghccps <167923020+ghccps@users.noreply.github.com>
2024-05-06 00:25:53 -0300

    Corrected formatting

--- src/main/java/com/vehicle/car/model/Car.java
+++ src/main/java/com/vehicle/car/model/Car.java
@@ -41,9 +41,10 @@ public class Car {
 
 	protected void printCarData() {
 		System.out.println(
-				"Placa: " + licensePlate +
-						"\n" +
-						"Ano: " + year);
+						   "Placa: " + licensePlate +
+						   "\n" +
+						   "Ano: " + year
+						  );
 	}
 
 }
